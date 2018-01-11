c492f0625381f13eae0378b28ca8bd1e2b5bb4cb
soleynenero <soleyne.nero@etu.u-pec.fr>
2018-01-11 15:15:28 +0100

    mise en place du cote back et front

--- front/src/route.php
+++ front/src/route.php
@@ -8,62 +8,95 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
+
+/*** ROUTES DE LA NAV ET DE LA PAGE D'ACCUEIL***/
+
+// route Home
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array());
-})
-->bind('homepage')
-;
+})->bind('Home');
+
+// route roomtarif
+$app->get('/roomtarif', function () use ($app) {
+    return $app['twig']->render('basic/room_tarif.html.twig', array());
+})->bind('roomtarif');
+
+// route introduction
+$app->get('/introduction', function () use ($app) {
+    return $app['twig']->render('basic/introduction.html.twig', array());
+})->bind('introduction');
+
+// route gallery
+$app->get('/gallery', function () use ($app) {
+    return $app['twig']->render('basic/gallery.html.twig', array());
+})->bind('gallery');
+
+// route contacts
+$app->get('/contacts', function () use ($app) {
+    return $app['twig']->render('basic/contacts.html.twig', array());
+})->bind('contacts');
+
+// route mentions_legales et CGV
+$app->get('/mentions_legales', function () use ($app) {
+    return $app['twig']->render('basic/mentions_legales.html.twig', array());
+})->bind('mentions_legales');
 
 
-
-
-// route coté front
+/*** CONNEXION ET INSCRIPTION ***/
 
 // page d'inscription
 $app->get('/inscription', function() use($app)
 {
+    return $app['twig']->render('basic/inscription.html.twig', array());
 // peut etre mettre un lien vers la page login si l'utilisateur a deja un compte
-});
-
+})->bind('inscription');
+ 
 $app->post('/inscription', function() use($app)
 {
-// mdp , nom , prenom ,  adresse , ville , code postal , tel , email , statut en hidden
+// Post à compléter avec les conditions de contrôle : mdp , nom , prenom ,  adresse , ville , code postal , tel , email , statut en hidden
 });
 
 
 // page de connexion
 $app->get('/connexion', function() use($app)
 {
-
-});
+    return $app['twig']->render('basic/connexion.html.twig', array());
+})->bind('connexion');
 
 $app->post('/connexion', function() use($app)
 {
-// mdp , token en hidden ( a générer a l'ouverture de la session)
+// Post à compléter : mdp , token en hidden ( a générer a l'ouverture de la session)
 });
 
+// page mot de passe oublié
+$app->post('/oubli_mdp', function() use($app)
+{
+    return $app['twig']->render('basic/oubli_mdp.html.twig', array());
+})->bind('oubli_mdp');
+
+/*** LES ROUTES DES PAGES SECONDAIRES ***/
 
 // page profil
 $app->get('/profil_membre', function() use($app)
 {
-
-});
+    return $app['twig']->render('basic/profil_membre.html.twig', array());
+})->bind('profil_membre');
 
 $app->post('/profil_membre', function() use($app)
 {
-// nom , prenom ,  adresse , ville , code postal , tel , email , modif mdp
+// Post à compléter : nom , prenom ,  adresse , ville , code postal , tel , email , modif mdp
 });
 
 
 // page de chambre standard
 $app->get('/chambre_standard', function() use($app)
 {
-
-});
+    return $app['twig']->render('basic/chambre_standard.html.twig', array());
+})->bind('chambre_standard');
 
 $app->post('/chambre_standard', function() use($app)
 {
-// date deb, date fin, nb personnes ,prix , categorie en hidden , capacité , nom service
+// Post à compléter : date deb, date fin, nb personnes ,prix , categorie en hidden , capacité , nom service
 // lien vers le module de paiement ( API)
 });
 
@@ -71,12 +104,12 @@ $app->post('/chambre_standard', function() use($app)
 // page de chambre supérieur
 $app->get('/chambre_superieure', function() use($app)
 {
-
-});
+    return $app['twig']->render('basic/chambre_superieure.html.twig', array());
+})->bind('chambre_superieure');
 
 $app->post('/chambre_superieure', function() use($app)
 {
-// date deb, date fin, nb personnes ,prix , categorie en hidden , capacité , nom service
+// Post à compléter : date deb, date fin, nb personnes ,prix , categorie en hidden , capacité , nom service
 // lien vers le module de paiement ( API)
 });
 
@@ -84,12 +117,12 @@ $app->post('/chambre_superieure', function() use($app)
 // page de chambre luxe
 $app->get('/chambre_luxe', function() use($app)
 {
-
-});
+    return $app['twig']->render('basic/chambre_luxe.html.twig', array());
+})->bind('chambre_luxe');
 
 $app->post('/chambre_luxe', function() use($app)
 {
-// date deb, date fin, nb personnes ,prix , categorie en hidden , capacité , nom service 
+// Post à compléter : date deb, date fin, nb personnes ,prix , categorie en hidden , capacité , nom service 
 // lien vers le module de paiement ( API)
 });
 
@@ -98,15 +131,18 @@ $app->post('/chambre_luxe', function() use($app)
 $app->get('/validation', function() use($app)
 {
 // numéro facture, prix total ,  moyen de paiement , la date  
-});
+    return $app['twig']->render('basic/validation.html.twig', array());
+})->bind('validation');
 
 
-// page service
+// page services
 $app->get('/services', function() use($app)
 {
+    return $app['twig']->render('basic/services.html.twig', array());
+})->bind('services');
 
-});
 
+// pages ERREURS
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
