40264e1ca1bc84ada88755cbaf37c2a1470718d8
soleynenero <soleyne.nero@etu.u-pec.fr>
2018-01-19 14:18:42 +0100

    modification du statut

--- src/Model/GestionChambreDAO.php
+++ src/Model/GestionChambreDAO.php
@@ -21,10 +21,11 @@
 
         public function selectChambre()
         {
-            $sql = "SELECT c.id_chambres ,c.numero_chambre , cat.type_categorie AS 'Categorie', cap.capacite , c.telephone ,c.prix , c.statut
-            FROM capacite_chambre cap , categorie_chambre cat , chambres c
+            $sql = "SELECT c.id_chambres ,c.numero_chambre , cat.type_categorie AS 'Categorie', cap.capacite , c.telephone ,c.prix , s.statut
+            FROM capacite_chambre cap , categorie_chambre cat , chambres c , statut s
             WHERE cat.id_categorie = c.id_categorie
             AND cap.id_capacite = c.id_capacite
+            AND s.id_statut = c.id_statut
             ORDER BY c.numero_chambre";
             $chambre = $this->db->fetchAll($sql);
             return $chambre ;
@@ -58,30 +59,30 @@
             return $chambre ;
         }
 
-        public function selectmodifChambre($id_chambres)
-        {
-            $sql = "SELECT c.id_chambres ,c.numero_chambre , cat.type_categorie AS 'Categorie', cap.capacite , c.telephone ,c.prix , c.statut
-            FROM capacite_chambre cap , categorie_chambre cat , chambres c
-            WHERE cat.id_categorie = c.id_categorie
-            AND cap.id_capacite = c.id_capacite
-            AND c.id_chambres = ? ";
-            $chambre = $this->db->fetchAssoc($sql, array((int) $id_chambres));
-            return $chambre ;
-        }
+        // public function selectmodifChambre($id_chambres)
+        // {
+        //     $sql = "SELECT c.id_chambres ,c.numero_chambre , cat.type_categorie AS 'Categorie', cap.capacite , c.telephone ,c.prix , c.statut
+        //     FROM capacite_chambre cap , categorie_chambre cat , chambres c
+        //     WHERE cat.id_categorie = c.id_categorie
+        //     AND cap.id_capacite = c.id_capacite
+        //     AND c.id_chambres = ? ";
+        //     $chambre = $this->db->fetchAssoc($sql, array((int) $id_chambres));
+        //     return $chambre ;
+        // }
 
-        public function modifChambre($numero_chambre , $id_categorie , $id_capacite , $telephone , $prix , $statut, $id_chambres)
-        {
-            $sql = "UPDATE chambres SET numero_chambre = ?, id_categorie = ?, id_capacite = ?, telephone = ?, prix = ?, statut  WHERE id_chambres = ?";
+        // public function modifChambre($numero_chambre , $id_categorie , $id_capacite , $telephone , $prix , $statut, $id_chambres)
+        // {
+        //     $sql = "UPDATE chambres SET numero_chambre = ?, id_categorie = ?, id_capacite = ?, telephone = ?, prix = ?, statut  WHERE id_chambres = ?";
 
-            $chambremodif = $this->db->executeUpdate($sql, array($numero_chambre , $id_categorie , $id_capacite , $telephone , $prix , $statut, (int) $id_chambres));
-            return $chambremodif;
-        }
+        //     $chambremodif = $this->db->executeUpdate($sql, array($numero_chambre , $id_categorie , $id_capacite , $telephone , $prix , $statut, (int) $id_chambres));
+        //     return $chambremodif;
+        // }
 
-        public function deleteChambre($id_chambres)
-        {
-            $bdd = $this->getDb() ;
-            $deletechambre = $bdd->delete('chambres', array(
-                "id_chambres" => $id_chambres)) ;
-            return $deletechambre;
-        }
+        // public function deleteChambre($id_chambres)
+        // {
+        //     $bdd = $this->getDb() ;
+        //     $deletechambre = $bdd->delete('chambres', array(
+        //         "id_chambres" => $id_chambres)) ;
+        //     return $deletechambre;
+        // }
     }
