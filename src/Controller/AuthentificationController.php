69be42dd4cde896d530ab6b8ca63c79e0ab0cd20
soleynenero <soleyne.nero@etu.u-pec.fr>
2018-01-22 17:36:10 +0100

    mise en place du front

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -22,32 +22,32 @@
                 $errors = "";
 
                 if(iconv_strlen($prenom) < 2 || iconv_strlen( $prenom )> 15){
-                 $errors .= ' Erreur de la taille de votre prenom  ;'   ;
+                 $errors .= " Erreur de la taille de votre prenom \n "   ;
                 
                 
                 
                  }
                 if(iconv_strlen($nom) < 2 || iconv_strlen($nom) > 15){
-                 $errors .= ' Erreur de la taille de votre nom  ;';
+                 $errors .= " Erreur de la taille de votre nom \n  ";
                 
                
                 }
     
                 if(!is_numeric($codepostal) || iconv_strlen($codepostal) != 5){
-                $errors .= ' Erreur de format  du code postal  ;';
+                $errors .= " Erreur de format  du code postal  \n ";
                 
                 
                  }
     
                  if(!is_numeric( $telephone) || iconv_strlen( $telephone) != 10){
-                    $errors .= ' Erreur du  format du telephone    ;';
+                    $errors .= "Erreur du  format du telephone \n";
                    
                     
                  }          
             
                 if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     
-                    $errors .= ' Erreur du format du champs email   ; ';
+                    $errors .= "Erreur du format du champs email \n ";
                    
                 }
                
